d2b7913096b80c86fd3dbf562991f7a27edc74fb
Riyaz J <jriyaz@zeomega.com>
2022-07-27 13:23:05 +0530

    changes in api names

--- session-testing.js
+++ session-testing.js
@@ -1,10 +1,11 @@
-import { sleep, group } from "k6";
+import { sleep, group, check } from "k6";
 import http from "k6/http";
 import { randomItem, randomIntBetween, randomString } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
 // const BASE_URL = "http://172.27.1.137:5001/";// Server
-const BASE_URL = "http://127.0.0.1:5001/";
-const LETTERS_URL = `${BASE_URL}letters/api/`;
+// const BASE_URL = "http://172.31.128.1:5001/";
+const BASE_URL = "http://127.0.0.1:5000/";
+const LETTERS_URL = `${BASE_URL}correspondence/`;
 const CREDENTIALS = { username: "zeadmin", password: "Jiva@123" };
 
 const episode_types = ['Appeal', 'BH-CM', 'BH-IP', 'BH-OP', 'CM', 'DM', 'ISSUES', 'HP', 'IP', 'LCN', 'LTSS', 'MA-Appeal', 'MA-Grievance', 'MTM', 'MRIP', 'OP', 'EP', 'PR', 'QR'];
@@ -27,87 +28,64 @@ const payor_names = ['best', 'internal', 'test', 'ama', 'abc'];
 const keyword_idns = [7, 8, 38, 56, 57, 5000006, 43, 5000018, 106, 5000006, 107, 182];
 const keyword_cds = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const notice_idns = [97, 104, 135, 142, 144, 147, 152, 153, 12325, 35328, 36306, 36741, 44726, 44730];
-const dummy_enc_idns = [12325, '-1', 35328, '-1', 36306, '-1', 36741, '-1', 44726, '-1', 44730];
+const dummy_notice_idns = [12325, '-1', 35328, '-1', 36306, '-1', 36741, '-1', 44726, '-1', 44730];
 const template_master_idns = [10721, 10722, 10724, 10725, 10728, 10729, 10730, 10731, 10733, 10736];
-const dummy_mbr_idns = [162387, 159950, 158452, 191905, 174089, 185318, 173395, 172976, 162387, 175758];
-const start_date = ['04%2F03%2F2021', '12%2F12%2F2021', '06%2F21%2F2021', '08%2F12%2F2021'];
-const end_date = ['04%2F13%2F2022', '03%2F13%2F2022', '02%2F13%2F2022', '01%2F13%2F2022'];
-const prv_idn = [138115, 153862, 154120];
-const prv_addr_idn = [595447, 596215, 596243];
-const yesno = ['Y', 'N'];
-const program_idn = [42, 252, 5012646];
 // const attach_pdf = [{'pdf_name': 'seladmin_54f2f0f718804982aaf93f6a7dd63b29', 'idn': 92}, {'pdf_name': 'seladmin_0d99fa8458c44ce390fddca14f306b7e', 'idn': 93}]
 
 export const options = {
-    // vus: 1,
-    // duration: "2m",
-    // iterations: 5,
-    // stages: [
-    //     { target: 20, duration: "30s" },
-    //     { target: 10, duration: "2m" },
-    //     { target: 0, duration: "0" },
-    // ],
-
+    vus: 2,
+    duration: "30s",
     thresholds: {
         http_req_failed: ["rate < 0.1"], // http errors should be less than 1%
         http_req_duration: ["p(95) < 200"], // 95% of requests should be below 200ms
     },
 };
 export function setup() {
-    const loginRes = http.post(`${BASE_URL}auth`, JSON.stringify(CREDENTIALS), {
+    const loginRes = http.post(`${LETTERS_URL}auth`, JSON.stringify(CREDENTIALS), {
         headers: { "Content-Type": "application/json" },
     });
 
     const authToken = loginRes.json("access_token");
-    // check(authToken, {
-    //   "logged in successfully": () => authToken !== "Invalid Credentials",
-    // });
-    console.log(authToken);
+    check(authToken, {
+        "logged in successfully": () => authToken !== "Invalid Credentials",
+    });
+    return authToken;
 
     // return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY0NDQ5OTcxNiwianRpIjoiemVhZG1pbiIsInR5cGUiOiJhY2Nlc3MiLCJuYmYiOjE2NDQ0OTk3MTYsImNzcmYiOiIzYmM2ZmJlMy0xMzliLTQ3MTAtYWNhMy0yOTBlNTRhMzliMWUiLCJleHAiOjE2NDcwOTE3MTYsInVzZXJuYW1lIjoiemVhZG1pbiIsInJvbGVzIjpbXX0.pIY0wX3Suou_rsUisXbp7Cshhhj1wuVTEgIeYN-hMSc";
-    return authToken;
 }
 
 export default (authToken) => {
-    const bearerToken = {
+    const bearer_token = {
         headers: {
             Authorization: `Bearer ${authToken}`,
             "Content-Type": "application/json",
         },
     };
-
-
-    // http.post(
-    //     `${LETTERS_URL}notif_templ_activate_deactivate`,
-    //     JSON.stringify({ ACTION_NOTICE_IDN: randomItem(notice_idns), ENTITY_ACTIVE: randomItem(is_all) }),
-    //     bearerToken
-    // );
+    group('Template | Add or Modify', function () {
+        http.batch([
+            ["GET", `${LETTERS_URL}templates/prefills`, null, bearer_token],
+            ["GET", `${LETTERS_URL}contact-types`, null, bearer_token],
+            ["GET", `${LETTERS_URL}letter-tags?letter_type=${randomItem(letter_types)}&enc_type=${randomItem(episode_types)}`, null, bearer_token],
+            ["GET", `${LETTERS_URL}payors?query=${randomItem(payor_names)}&max_results=${randomIntBetween(1, 10)}&payor_access=P`, null, bearer_token],
+            ["GET", `${LETTERS_URL}employers?query=test&payor_ids=5347,89328,5349,84424,111&max_results=10&payor_access=P`, null, bearer_token],
+            ["GET", `${LETTERS_URL}groups?query=test&emp_ids=60036,56891,189,56891&max_results=10`, null, bearer_token],
+            ["GET", `${LETTERS_URL}episode-types`, null, bearer_token],
+            ["GET", `${LETTERS_URL}decisions?enc_type=${randomItem(episode_types)}`, null, bearer_token],
+            ["GET", `${LETTERS_URL}episode-types/outreach`, null, bearer_token],
+            ["GET", `${LETTERS_URL}programs?enc_type=${randomItem(episode_types)}`, null, bearer_token],
+            ["GET", `${LETTERS_URL}provider-address-types`, null, bearer_token],
+            ["GET", `${LETTERS_URL}provider-fax-types`, null, bearer_token],
+            ["GET", `${LETTERS_URL}provider-roles`, null, bearer_token],
+            ["GET", `${LETTERS_URL}sent-to`, null, bearer_token],
+            ["GET", `${LETTERS_URL}states`, null, bearer_token],
+            ["GET", `${LETTERS_URL}templates/types`, null, bearer_token]
+        ]);
+    });
 
     group('Template | Attach PDF', function () {
         http.batch([
-            ["POST", `${LETTERS_URL}attach_pdf_list`,
-                JSON.stringify({
-                    noticeIdn: randomItem(notice_idns),
-                    pdfAttachedList: [
-                        {
-                            DOC_NAME: "seladmin_aa48930337c2481d8688cb3c97a97526",
-                            EDU_MATERIAL_IDN: 1164,
-                        },
-                        {
-                            DOC_NAME: "zeadmin_495fd6c2e6504c19a8b99fea38735897",
-                            EDU_MATERIAL_IDN: 1194,
-                        },
-                        {
-                            DOC_NAME: "seladmin_54f2f0f718804982aaf93f6a7dd63b29",
-                            EDU_MATERIAL_IDN: 92,
-                        },
-                        {
-                            DOC_NAME: "seladmin_0d99fa8458c44ce390fddca14f306b7e",
-                            EDU_MATERIAL_IDN: 93,
-                        },
-                    ],
-                }), , bearerToken]
-            // ["GET", `${LETTERS_URL}get_attched_pdfs?noticeIdn=${randomItem(notice_idns)}`, null, bearerToken]
-            ]);
+            ["GET", `${LETTERS_URL}pdfs/${randomItem(notice_idns)}`, null, bearer_token],
+            ["GET", `${LETTERS_URL}pdfs?notice_id=${randomItem(notice_idns)}&page=${randomIntBetween(1, 5)}`, null, bearer_token],
+        ]);
     });
 };
